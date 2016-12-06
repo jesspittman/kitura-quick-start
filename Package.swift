e2eb0bb3781a961d69f9a4f616a555dc53143d85
Jessica Pittman <jpittma@us.ibm.com>
2016-12-06 15:51:48 -0600

    HeliumLogger to log informative kitura things.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "kitura-quick-start",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,9 @@
 import Foundation
 import Kitura
+import HeliumLogger
 
 let appRouter = AppRouter()
+HeliumLogger.use(.info)
     
 Kitura.addHTTPServer(onPort: 8090, with: appRouter.router)
 Kitura.run()
